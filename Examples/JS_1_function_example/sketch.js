be22720166fd19e24a0bdbc5d2e4d5707db04892
Chelley Sherman <chelleysherman@gmail.com>
2016-06-15 17:12:40 -0700

    Update sketch.js

--- Examples/JS_1_function_example/sketch.js
+++ Examples/JS_1_function_example/sketch.js
@@ -1,20 +1,14 @@
-var radius = 20;
-
 function setup() {
   createCanvas(500,500);
-  background(255,0,0);
+  background(0);
   
 }
 
 function draw() {
-  //background(100,100);
+
   fill(255);
   
   
   rect(0,0,50,80);
-  print(mouseY);
-  
-  
-  fill(0,255,0);
-  //ellipse(mouseX,mouseY,70,70);
+
 }
